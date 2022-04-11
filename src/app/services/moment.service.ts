5484b299fe0b2c8532caca6cfe906f7766df73d0
cristian.etchebest <cristian.etchebest@meta.com.br>
2022-04-11 17:40:25 -0300

    Campo pesquisa

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,17 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Moment } from '../interfaces/Moment';
 import { environment } from 'src/environments/environment';
+import { Response } from './../interfaces/Response';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MomentService {
   private baseUrl = environment.baseApiUrl;
-  private apiUrl = `${this.baseUrl}/moments`;
+  private apiUrl = `${this.baseUrl}api/moments`;
 
   constructor(private http: HttpClient) {}
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
+  }
+
+  getMoments(): Observable<Response<Moment[]>>{
+    return this.http.get<Response<Moment[]>>(this.apiUrl)
   }
 }
